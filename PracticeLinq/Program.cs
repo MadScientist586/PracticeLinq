715f8e04c6dcddc13973d77e859ba57383614c7d
MadScientist586 <mr.scientist.knight@gmail.com>
2022-09-30 10:55:37 -0500

    made list of video games

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,17 @@
+﻿List<string> VideoGames = new()
+{ "Splatoon 3",
+    "Zelda",
+    "Minecraft",
+    "Jedi: Fallen Order",
+    "ClusterTruck",
+    "Fall Guys",
+    "Rec Room",
+    "Roblox",
+    "Super Mario Bros.",
+    "Destiny 2"
+};
+VideoGames.OrderBy(VideoGames => VideoGames.Length)
+    .ThenBy(VideoGames => VideoGames)
+    .ToList()
+    .ForEach(VideoGames => Console.WriteLine(VideoGames));
+
